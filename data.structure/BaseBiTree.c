de54f468c02bd603844f0242ca2f4c2b9af0e73f
rongbin.xie <rongbin.xie@voyageone.cn>
2019-03-04 19:06:16 +0800

    修改一些c 例子

---
+++ data.structure/BaseBiTree.c
@@ -0,0 +1,37 @@
+//
+// Created by dell on 2019/3/4.
+//
+#include <stdio.h>
+#include <corecrt_malloc.h>
+#include <stdlib.h>
+
+typedef struct TElemType {
+
+} TElemType;
+
+typedef struct BiTNode {
+    char data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+int OVERFLOW = -1;
+
+/**
+ * 按前序输入二叉树中节点的值（一个字符）
+ * #表示空树，构造二叉链表表示二叉树T
+ * @param T
+ */
+void createBiTree(BiTree *T) {
+    char ch;
+    scanf("%c", &ch);
+    if (ch == '#') {
+        *T = NULL;
+    } else {
+        *T = (BiTree) malloc(sizeof(BiTNode));
+        if (!*T) exit(OVERFLOW);
+
+        (*T)->data = ch;
+        createBiTree(&(*T)->lchild);
+        createBiTree(&(*T)->rchild);
+    }
+}
--- data.structure/main.c
+++ data.structure/main.c
@@ -2,8 +2,8 @@
 // Created by dell on 2019/3/4.
 //
 #include <stdio.h>
-#include "BiTree02.h"
 
 int main(int argc, char **argv) {
-
+//    return runBiTree();
+    return 1;
 }
