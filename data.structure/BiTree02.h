8bf3e20546c9d3d6efedfbe9ebf6bb5e20a202df
rongbin.xie <rongbin.xie@voyageone.cn>
2019-03-04 16:15:08 +0800

    c test case

---
+++ data.structure/BiTree02.h
@@ -0,0 +1,10 @@
+//
+// Created by dell on 2019/3/4.
+//
+
+#ifndef BI_TREE_BITREE02_H
+#define BI_TREE_BITREE02_H
+
+#endif //BI_TREE_BITREE02_H
+
+extern int runBiTree();
---
+++ data.structure/PointAddr.c
@@ -0,0 +1,9 @@
+//
+// Created by dell on 2019/3/4.
+//
+#include <stdio.h>
+#include "BiTree02.h"
+
+int main(int argc, char **argv) {
+    runBiTree();
+}
